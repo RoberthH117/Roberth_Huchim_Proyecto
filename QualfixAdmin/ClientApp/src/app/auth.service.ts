0abeafeb4672220bcadd95a0172cb2a6cee4d4a4
RoberthH117 <roberthhuchim1@gmail.com>
2025-08-09 21:02:22 -0600

    add  auth

--- QualfixAdmin/ClientApp/src/app/auth.service.ts
+++ QualfixAdmin/ClientApp/src/app/auth.service.ts
@@ -17,9 +17,9 @@ export class AuthService implements CanActivate{
     return this.apiservice.GetComprobation().pipe(
       catchError((error) => {
         // En caso de error al comprobar la autenticación, redirige al inicio de sesión.
-        // this.router.navigate(['/login']);
+        this.router.navigate(['/login']);
         console.log(error);
-        return of(true);
+        return of(false);
       })
     );
   }
